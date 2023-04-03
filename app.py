be690642ab8b99268ea50be4c85ee6b99bbc689f
ilesanmi-007 <ilesanmi3451@gmail.com>
2023-04-03 13:23:25 +0100

    editted well and reran the app.py. corrected errors to ensure it runs well

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import pickle
 import streamlit as st
 
-pickle_in = open('modelmodel2.pkl', 'rb')
-
+pickle_in = open('modelmodel.pkl', 'rb')
 clf = pickle.load(pickle_in)
 
-
-@st.cache()
-
+#@st.cache()
+@st.cache_data()
 def make_prediction(bhk, Bathroom, Size, City):
+    city_enc = -1
     if City == 'Mumbai':
         city_enc = 5
     elif City == 'Chennai':
@@ -21,37 +20,42 @@ def make_prediction(bhk, Bathroom, Size, City):
         city_enc = 2
     elif City == 'Kolkata':
         city_enc = 4
-    
+
     prediction = clf.predict([[bhk, Bathroom, Size, city_enc]])[0]
     return prediction
+ 
+#print(make_prediction('5', '6', '90000', 'Bangalore'))
 
 
 def main():
     #front end elements
     html_temp = """
-    <div style = "background-color:green;padding:10px">
-    <h1 style = "color:white;text-align:center;"> House Prices Prediction by Ilesanmi </h1>
+    <div style="background-color:green;padding:13px">
+    <h1 style="color:white;text-align:center;"> House Prices Prediction by Ilesanmi </h1>
     </div>
     """
-
     #front end 
-    st.markdown("![](https://cdn.modernghana.com/story_/780/420/424201721206_6_222124n01_0.jpg)")
-    
-    st.markdown(html_temp, unsafe_allow_html = True)
+    #st.markdown("![](https://thumbs.dreamstime.com/z/house-rent-down-town-35897819.jpg)")
+    #using image instead
+    from PIL import Image
 
+    img = Image.open("house-rent.jpg")
+    resized_img = img.resize((800, 300))
+    st.image(resized_img, caption="House for rent in downtown")
+
+
+    st.markdown(html_temp, unsafe_allow_html=True)
     #following lines create the visuals
-    City = st.selectbox('City', ('Mumbai','Chennai','Bangalore','Hyderabad','Delhi','Kolkata'))    
+    City = st.selectbox('City', ('Mumbai', 'Chennai', 'Bangalore', 'Hyderabad', 'Delhi', 'Kolkata'))    
     Bathroom = st.number_input('Enter number of Bathroom')
-    Size = st.number_input('Enter Size of the Land in square Feet (900 sqm is a plot)')
-    bhk = st.number_input('Enter number of Bedroom, Hall and Kitchen')
+    Size = st.number_input('Enter Size of the Land')
+    bhk = st.number_input('Enter number of BHK')
 
     #when 'predict' is clicked, make prediction
     if st.button('Make Prediction'):
         result = make_prediction(bhk, Bathroom, Size, City)
-        st.success('The price is mostlikely: {}'.format(result))
-        print('Just test')
+        st.success('The price is most likely: {}'.format(result))
 
 if __name__ == '__main__':
     main()
-
-
+   
